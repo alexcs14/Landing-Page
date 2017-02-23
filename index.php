73ba4afaa7c0d0f123a7b051d81b10c0cd5942fb
yenilpz <shatanoga.yp@gmail.com>
2017-02-23 09:51:40 -0500

    mision y vision 1
    ubicacion de mision y vision con imagen, aun falta modificar los
    parrafos

--- index.php
+++ index.php
@@ -4,12 +4,31 @@
     <meta charset="utf-8">
     <title></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
-    <link rel="stylesheet" href="assets/main.css">
+    <link rel="stylesheet" href="assets/stylesheet/main.css">
   </head>
-  <body>
+  <body class="mys">
+
+    <img src="assets/images/font-mys.jpg" >
+    <div class="row">
+        <div class="col s2"  id="mis" >
+          <h1 >Misión</h1>
+        </div>
+        <div class="col s2" id="vis">
+          <h1 >Visión</h1>
+        </div>
+    </div>
+    <div class="row" >
+      <div class="col s2" id="mys-txt1">
+        <p>La aplicación sera distribuida entre las distribuida entre las diferentes regiones de colombia, brindando un servicio optimo y eficaz en las diferentes donaciones que puedan ser brindadas.</p>
+      </div>
+      <div class="col s2" id="mys-txt2">
+        <p>Nuestro objetivo es lograr una mayor agilidad en el proceso de donaciones voluntarias, y de este modo aumentar el numero de personas salvadas por la contribucion de la comunidad.</p>
+      </div>
+    </div>
 
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
     <script type="text/javascript" src="assets/main.js"></script>
   </body>
+
 </html>
