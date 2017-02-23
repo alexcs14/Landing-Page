d63380bbe476aef17931c2360a0bb5b021814503
alexcs14 <jaoquendo9@misena.edu.co>
2017-02-23 11:53:34 -0500

    pantalla1

--- index.php
+++ index.php
@@ -8,6 +8,59 @@
   </head>
 
   <body>
+    <div class="slider fullscreen">
+        <ul class="slides">
+          <li>
+            <img src="assets/images/3.jpg" >
+            <div class="caption center-align">
+              <div class="row light grey-text text-lighten-3" id="tg">
+                <div class="col s5" id="arr">
+                  <h2>Misión</h2>
+                  <p class="hi" id="ma">Nuestro objetivo es lograr una mayor agilidad en el proceso de donaciones voluntarias, y de este modo aumentar el numero de personas salvadas por la contribucion de la comunidad.</p>
+                </div>
+                    <div class="col s5" id="ma">
+                      <h2>Visión</h2>
+                      <p class="hi" id="ma">La aplicación sera distribuida entre las distribuida entre las diferentes regiones de colombia, brindando un servicio optimo y eficaz en las diferentes donaciones que puedan ser brindadas.</p>
+                    </div>
+                  </div>
+            </div>
+          </li>
+          <li>
+            <img src="assets/images/4.jpg">
+            <div class="caption left-align">
+              <div class="container row">
+                <h3>Quienes Somos</h3>
+                <div>
+                  <div>
+                    <p class="col5" id="ha">
+                      Somos un grupo de personas interesados por el tema de la salud publica, con ganas de ayudar y innovar en las
+                      tecnologias.
+                    </p>
+                  </div>
+                  <div>
+                    <p class="ha col5" id="ha">
+                      Nuestro objetivo es mostrar al mundo nuevas posibilidades de ayudar a quien lo necesita, suministrando una
+                      plataforma en la cual podran dar una mano por la salud de otro.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <img src="assets/images/6.jpg">
+            <div class="caption right-align row">
+              <h3>Objetivo</h3>
+              <p id="ha">
+                El objetivo de nuestra aplicación es avanzar a una nueva forma de realizar las donaciones, dandole
+                una herramienta por la cual los voluntarios tengan participación activa en el proceso, y de esta manera
+                generar solidaridad entre la comunidad.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
       <div class="container row grey">
         <h2>Información</h2>
         <p class="col s12">
@@ -28,41 +81,14 @@
             </p>
           </span>
         </div>
-        <img src="assets/images/1.jpg"/>
 
-    <img src="assets/images/font-mys.jpg" >
-    <div class="row">
-        <div class="col s2"  id="mis" >
-          <h1 >Misión</h1>
-        </div>
-        <div class="col s2" id="vis">
-          <h1 >Visión</h1>
-        </div>
-    </div>
-    <div class="row" >
-      <div class="col s2" id="mys-txt1">
-        <p>La aplicación sera distribuida entre las distribuida entre las diferentes regiones de colombia, brindando un servicio optimo y eficaz en las diferentes donaciones que puedan ser brindadas.</p>
-      </div>
-      <div class="col s2" id="mys-txt2">
-        <p>Nuestro objetivo es lograr una mayor agilidad en el proceso de donaciones voluntarias, y de este modo aumentar el numero de personas salvadas por la contribucion de la comunidad.</p>
-      </div>
-    </div>
 
-    <div class="container row">
-      <div class="col s5">
-        <h2>Quienes Somos</h2>
-        <p>
-          Somos un grupo de personas interesados por el tema de la salud publica, con ganas de ayudar y innovar en las
-          tecnologias.
-        </p>
-        <p>
-          Nuestro objetivo es mostrar al mundo nuevas posibilidades de ayudar a quien lo necesita, suministrando una
-          plataforma en la cual podran dar una mano por la salud de otro.
-        </p>
-      </div>
-    </div>
+
+
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-    <script type="text/javascript" src="assets/main.js"></script>
+    <script type="text/javascript" src="assets/js/main.js"></script>
   </body>
 
 </html>
