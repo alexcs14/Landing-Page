130d559a61ea6f54f958b44360cb1132b802000e
alexcs14 <jaoquendo9@misena.edu.co>
2017-02-23 09:53:37 -0500

    informacion

--- index.php
+++ index.php
@@ -7,7 +7,27 @@
     <link rel="stylesheet" href="assets/main.css">
   </head>
   <body>
-
+      <div class="container row grey">
+        <h2>Información</h2>
+        <p class="col s12">
+          La aplicación esta desarrollada para ser distribuida por las diferentes regiones de colombia brindando una
+          alternativa optima a la comunidad en el proceso de donaciones, haciendo de esté, un proceso más rapido
+          y accesible.</p>
+          <span class="col s6 left">
+            <p>
+              Acceso a la comunidad: La aplicación estara a la mano de los voluntarios, dandoles la posibilidad de ofrecer
+              sus aportes a las instituciones, para que estas realicen el debido proceso.
+            </p>
+          </span>
+          <span class="col s6 right">
+            <p>
+              Acceso a las instituciones de salud: En el caso de las entidades responsables de las donaciones, poseeran
+              una plataforma capaz de filtrar a los voluntarios según los requisitos que necesitan para determinado
+              paciente y/o situación.
+            </p>
+          </span>
+        </div>
+        <img src="assets/images/1.jpg"/>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
     <script type="text/javascript" src="assets/main.js"></script>
