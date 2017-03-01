717f5746f65bf7c5ab57c629a6f02492b31549d5
alexcs14 <jaoquendo9@misena.edu.co>
2017-03-01 07:22:20 -0500

    MVC

---
+++ controller/main.controller.php
@@ -0,0 +1,11 @@
+<?php
+class MainController{
+
+  public function main(){
+    require_once "views/include/header.php";
+    require_once "views/module/inicio/index.php";
+    require_once "views/include/footer.php";
+  }
+
+}
+?>
--- index.php
+++ index.php
@@ -1,99 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
-    <link rel="stylesheet" href="assets/stylesheet/main.css">
-  </head>
-
-  <body>
-    <div class="slider fullscreen">
-        <ul class="slides">
-          <li>
-            <img src="assets/images/3.jpg" >
-            <div class="caption center-align">
-              <div class="row light grey-text text-lighten-3" id="tg">
-                <div class="col s5" id="arr">
-                  <h2>Misión</h2>
-                  <p class="hi" id="ma">Nuestro objetivo es lograr una mayor agilidad en el proceso de donaciones voluntarias, y de este modo aumentar el numero de personas salvadas por la contribucion de la comunidad.</p>
-                </div>
-                    <div class="col s5" id="ma">
-                      <h2>Visión</h2>
-                      <p class="hi" id="ma">La aplicación sera distribuida entre las distribuida entre las diferentes regiones de colombia, brindando un servicio optimo y eficaz en las diferentes donaciones que puedan ser brindadas.</p>
-                    </div>
-                  </div>
-            </div>
-          </li>
-          <li>
-            <img src="assets/images/4.jpg">
-            <div class="caption left-align">
-              <div class="container row">
-                <h3>Quienes Somos</h3>
-                <div>
-                  <div>
-                    <p class="col5" id="ha">
-                      Somos un grupo de personas interesados por el tema de la salud publica, con ganas de ayudar y innovar en las
-                      tecnologias.
-                    </p>
-                  </div>
-                  <div>
-                    <p class="ha col5" id="ha">
-                      Nuestro objetivo es mostrar al mundo nuevas posibilidades de ayudar a quien lo necesita, suministrando una
-                      plataforma en la cual podran dar una mano por la salud de otro.
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
-          <li>
-            <img src="assets/images/6.jpg">
-            <div class="caption right-align row">
-              <h3>Objetivo</h3>
-              <p id="ha">
-                El objetivo de nuestra aplicación es avanzar a una nueva forma de realizar las donaciones, dandole
-                una herramienta por la cual los voluntarios tengan participación activa en el proceso, y de esta manera
-                generar solidaridad entre la comunidad.
-              </p>
-            </div>
-          </li>
-        </ul>
-
-        <div class="row">
-          <h2>Información</h2>
-          <p class="col s7">
-            La aplicación esta desarrollada para ser distribuida por las diferentes regiones de colombia brindando una
-            alternativa optima a la comunidad en el proceso de donaciones, haciendo de esté, un proceso más rapido
-            y accesible.</p>
-            <div class="col s6">
-              <span class="col s5 left">
-                <p>
-                  Acceso a la comunidad: La aplicación estara a la mano de los voluntarios, dandoles la posibilidad de ofrecer
-                  sus aportes a las instituciones, para que estas realicen el debido proceso.
-                </p>
-              </span>
-              <span class="col s5 right">
-                <p>
-                  Acceso a las instituciones de salud: En el caso de las entidades responsables de las donaciones, poseeran
-                  una plataforma capaz de filtrar a los voluntarios según los requisitos que necesitan para determinado
-                  paciente y/o situación.
-                </p>
-              </span>
-            </div>
-          </div>
-
-
-    <div class="container">
-        <div class="row">
-        <div class="col s3" id="cir-j" > <img class="circle responsive-img" src="assets/images/1.jpg">Jhony Alexander Oquendo Santana <span class="info"><p class="col s4" id="b"> Contacto: alex_os14@hotmail.com</p></span> </div>
-        <div class="col s3" id="cir-y" > <img class="circle responsive-img" src="assets/images/1.jpg">Yeny Paola López Salazar <span class="info"><p class="col s4" id="b"> Contacto: shatanoga.yp@gmail.com</p></span> </div>
-         </div>
-      </div>
-      <div id="terms">© Copyright 2017 by LifeSaver. All Rights Reserved. |  Terms</div>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
-    <script type="text/javascript" src="assets/js/main.js"></script>
-  </body>
-
-</html>
+<?php
+if (isset($_REQUEST["c"])){ //Se verificar si existe o mejor dicho, se esta llamando un controlador
+  $controller = strtolower($_REQUEST["c"]); //Acá se inicia el controlador
+  $action = isset($_REQUEST["a"]) ? $_REQUEST["a"]:"index"; //Y aquí la acción, en caso de no haber acción se inicia la funcion "Index"
+  require_once "controller/$controller.controller.php"; //Desde acá se llama al controlador en la carpeta
+  $controller = ucwords($controller).'Controller'; //Acá se llama a la clase de controllador
+  $controller = new $controller;
+  call_user_func(array($controller,$action));
+}else{ //En caso que no exista, pasa lo siguiente:
+  $controller = "main"; //Se llama al controlador "main", y este se guarda en la variable $controller
+  require_once "controller/$controller.controller.php"; //Llamamos al controlador "main" desde la carpeta
+  $controller = ucwords($controller).'Controller'; //Buscamos la clase
+  $controller = new $controller; //Instanciamos el controlador
+  $controller -> main(); //Y por ultimo llamamos la funcion "Main"
+}
+?>
