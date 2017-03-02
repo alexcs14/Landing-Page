c0c3a869239cf4fe03105201f976388ee0802fac
yenilpz <shatanoga.yp@gmail.com>
2017-03-02 17:36:01 -0500

    immys
    movi unas cosas de mision y vision y agrege unas imagenes que no me
    uisieron dar

--- views/module/menu/mision-vision.php
+++ views/module/menu/mision-vision.php
@@ -1,10 +1,17 @@
 <div class="row light grey-text text-lighten-3">
-  <div class="col s5" id="arr">
-    <h2>Misión</h2>
-    <p class="hi" id="ma">Nuestro objetivo es lograr una mayor agilidad en el proceso de donaciones voluntarias, y de este modo aumentar el numero de personas salvadas por la contribucion de la comunidad.</p>
+  <div class="col s5 center" id="arr">
+    <h2 class="ms">Misión</h2>
+    <p class="hi" >Nuestro objetivo es lograr una mayor agilidad en el proceso de donaciones voluntarias, y de este modo aumentar el numero de personas salvadas por la contribucion de la comunidad.</p>
   </div>
-      <div class="col s5" id="ma">
-        <h2>Visión</h2>
-        <p class="hi" id="ma">La aplicación sera distribuida entre las distribuida entre las diferentes regiones de colombia, brindando un servicio optimo y eficaz en las diferentes donaciones que puedan ser brindadas.</p>
-      </div>
+  <div class="col s5 center" id="img">
+  </div>
+</div>
+
+<div class="row light grey-text text-lighten-3">
+    <div class="col s5 center" id="ma">
     </div>
+    <div class="col s5 center" id="ma">
+      <h2 id="ma">Visión</h2>
+      <p class="hi" >La aplicación sera distribuida entre las distribuida entre las diferentes regiones de colombia, brindando un servicio optimo y eficaz en las diferentes donaciones que puedan ser brindadas.</p>
+    </div>
+</div>
