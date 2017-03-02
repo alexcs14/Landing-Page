9c7f64c4c6d586c843e838ed2aa932e90c5f592c
alexcs14 <jaoquendo9@misena.edu.co>
2017-03-01 20:24:07 -0500

    quenoseome

--- views/module/inicio/index.php
+++ views/module/inicio/index.php
@@ -20,6 +20,24 @@
       </div>
     </div>
 
+<div class="container row" id="base2">
+<span class="col s6">
+  <span class="dat"><h3>Participación activa</h3><p>
+    Acceso activo a la donación por parte de los voluntarios
+  </p></span>
+  <span class="dat"><h3>Mayor velocidad</h3><p>
+    Agilidad en el proceso de citación para las donaciones
+  </p></span>
+  <span class="dat"><h3>Apoyo a la comunidad</h3><p>
+    Brindar información sobre la donación a los usuarios
+  </p></span>
+  <span class="dat"><h3>Facilidad de uso</h3><p>
+    Multiples ayudas y amigable con el usuario
+  </p></span>
+</span>
+<span class="col s6"><img src="assets/images/celular.png" id="celular"></span>
+</div>
+
     <form class="" action="?c=main&a=mv" method="post">
       <button type="submit">enviar a misión y visión</button>
     </form>
